238ff0cc1ec7bbf9eb98964614ef2be8e7837a61
Ajay Kumar Mahto <165130037+ajaymahto15@users.noreply.github.com>
2024-12-21 09:55:47 +0000

    Postgres Initial Commit

---
+++ clusterstorm/commands.sh
@@ -0,0 +1,27 @@
+#sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+#curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+#sudo apt update
+#sudo apt install postgresql-17
+#sudo systemctl status postgresql
+#sudo service postgresql status
+#sudo service postgresql start
+
+#psql -V
+#psql (PostgreSQL) 17.2 (Ubuntu 17.2-1.pgdg20.04+1)
+
+#id postgres
+
+#CREATE USER npcidba WITH PASSWORD 'Npci@123'
+#CREATE database npcibank;
+#GRANT ALL PRIVILEGES ON DATABASE npcibank TO npcidba;
+
+CREATE TABLE customer (
+customer_id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(150) UNIQUE NOT NULL
+);
+
+INSERT INTO customer (name, email) VALUES ('John Doe', 'john.doe@example.com');
+
+# Peer Authentication by default
+# psql -U codespace -d npcibank -W 
